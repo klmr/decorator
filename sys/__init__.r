51877bee04a88aeb92cdc20a2cc51e5245411c55
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-01-19 16:18:57 +0000

    Modernise module "sys"

--- sys/__init__.r
+++ sys/__init__.r
@@ -1,4 +1,25 @@
+# Command line tools don’t want to clutter their output with unnecessary noise.
+library = function (...)
+    suppressMessages(base::library(...))
+
+#' The command line arguments
 args = commandArgs(trailingOnly = TRUE)
 
+#' Quit the program
+#'
+#' @param code numeric exit code (default: \code{0})
 exit = function (code = 0)
     quit(save = 'no', status = if (is.null(code)) 0 else code)
+
+#' Execute the \code{entry_point} function defined by the caller
+run = function (entry_point = main) {
+    caller = parent.frame()
+    caller_name = evalq(modules::module_name(), envir = caller)
+
+    if (is.null(caller_name)) {
+        if (class(substitute(entry_point)) == '{')
+            exit(entry_point)
+
+        exit(eval(substitute(main(), list(main = entry_point)), envir = caller))
+    }
+}
