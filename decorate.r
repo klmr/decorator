982cf2f39a0ff2f17a20ca2c9eb669a53674045d
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-09-07 00:23:09 +0100

    Store decorators in function attribute

--- decorate.r
+++ decorate.r
@@ -4,11 +4,16 @@
     stop(deparse(substitute(decorator)), ' is not a decorator')
 
 `%@%.decorator` = function (decorator, f) {
+    pretty_decorators = as.list(match.call())[-1]
+    # Patch delayed decorator.
+    if (! is.null({pretty_patched = attr(decorator, 'calls')}))
+        pretty_decorators = c(pretty_patched, pretty_decorators[-1])
+
     # Handle operator precedence so that we can chain decorators.
     if (inherits(f, 'decorator'))
-        .delayed_decorate(decorator, f)
+        .delayed_decorate(decorator, f, pretty_decorators)
     else
-        prettify(decorator(f), f)
+        prettify(decorator(f), f, pretty_decorators[-length(pretty_decorators)])
 }
 
 decorator = function (f)
@@ -17,8 +22,10 @@ decorator = function (f)
 decorator = decorator(decorator)
 
 
-prettify = function (f, original) {
+prettify = function (f, original, decorator_calls) {
     attr(f, 'srcref') = pretty_code(original)
+    attr(f, 'decorators') = decorator_calls
+    class(f) = c('decorated', class(f))
     f
 }
 
@@ -27,5 +34,5 @@ pretty_code = function (f) {
     if (is.null(srcref)) body(f) else srcref
 }
 
-.delayed_decorate = function (d1, d2)
-    decorator(function (f) d1(d2(f)))
+.delayed_decorate = function (d1, d2, decorator_calls)
+    structure(decorator(function (f) d1(d2(f))), calls = decorator_calls)
